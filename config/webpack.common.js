e4e8e4321d7c8d19736bbad7e8a1a8865e6d32cd
xxxsimons <974393900@qq.com>
2018-10-23 16:33:30 +0800

    [Bug] 修复在地址栏输入多层url报错

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -83,10 +83,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: false,
       template: HtmlWebpackTemplate,
-
+      baseHref: '/', // 相当于<base href="/">，为了解决直接输入'url'出现报错的Bug。参考：https://stackoverflow.com/questions/45133342/nested-react-router-4-routes-not-working-on-webpack-3?answertab=active#tab-top
       title: '杭州公交电动车管理平台',
       appMountId: 'app',
-      links: ['//at.alicdn.com/t/font_881369_p7ak1sxfd9a.css'],
+      links: ['http://at.alicdn.com/t/font_881369_p7ak1sxfd9a.css'],
     }),
   ],
 
