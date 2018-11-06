a99174edcc8ffc11e34d40da66cd7ef0930c5176
xxxsimons <974393900@qq.com>
2018-11-06 22:52:09 +0800

    [saga] 增加Map页面saga配置

--- src/saga.js
+++ src/saga.js
@@ -3,7 +3,8 @@ import Index from './Index/saga';
 import BusMonitor from './BusMonitor/saga';
 import BusManagement from './BusManagement/saga';
 import BusDetail from './BusDetail/saga';
+import Map from './Map/saga';
 
 export default function* rootSaga() {
-  yield all([Index(), BusMonitor(), BusManagement(), BusDetail()]);
+  yield all([Index(), BusMonitor(), BusManagement(), BusDetail(), Map()]);
 }
