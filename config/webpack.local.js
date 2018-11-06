4eb96f3813f7bcf82c814b8d3e6e5f6538f08ca1
xxxsimons <974393900@qq.com>
2018-11-06 22:54:29 +0800

    [core] 地图的基本配置

--- config/webpack.local.js
+++ config/webpack.local.js
@@ -21,6 +21,8 @@ module.exports = merge(common, {
         BASE_URL: JSON.stringify(
           'http://mock.eolinker.com/uNrvt624c0a75c8cb56b26bfca28ef23c58432f49216be5?uri=',
         ),
+        AMAP_KEY: JSON.stringify('e15e343ed5f952efd7899005dc60900f'),
+        AMAP_STYLE: JSON.stringify('amap://styles/47cf2cc474c7ad6c92072f0b267adff0?isPublic=true'),
       },
     }),
   ],
