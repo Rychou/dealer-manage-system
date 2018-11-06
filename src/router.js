ecc34dfefc3ccb618c5c3112ff8657b7d5833ecb
xxxsimons <974393900@qq.com>
2018-11-06 22:51:54 +0800

    [router] 增加Map页面路由配置

--- src/router.js
+++ src/router.js
@@ -7,7 +7,6 @@ import { Spin } from 'antd';
 import injectAsyncReducer from './injectAsyncReducer';
 import Layout from './Layout';
 import { Exception } from 'ant-design-pro';
-
 /* Router with lazy loaded pages. */
 class Router extends React.Component {
   static contextTypes = {
@@ -19,55 +18,42 @@ class Router extends React.Component {
 
     this.IndexPage = lodable({
       loader: () => {
-        injectAsyncReducer(
-          context.store,
-          'Index',
-          require('./Index/reducer').default,
-        );
+        injectAsyncReducer(context.store, 'Index', require('./Index/reducer').default);
         return import('./Index/container');
       },
       loading: () => <Spin spinning />,
     });
+    this.MapPage = lodable({
+      loader: () => {
+        injectAsyncReducer(context.store, 'Map', require('./Map/reducer').default);
+        return import('./Map/container');
+      },
+      loading: () => <Spin />,
+    });
     this.BusMonitor = lodable({
       loader: () => {
-        injectAsyncReducer(
-          context.store,
-          'BusMonitor',
-          require('./BusMonitor/reducer').default,
-        );
+        injectAsyncReducer(context.store, 'BusMonitor', require('./BusMonitor/reducer').default);
         return import('./BusMonitor/container');
       },
       loading: () => <Spin />,
     });
     this.BusDetail = lodable({
       loader: () => {
-        injectAsyncReducer(
-          context.store,
-          'BusDetail',
-          require('./BusDetail/reducer').default,
-        );
+        injectAsyncReducer(context.store, 'BusDetail', require('./BusDetail/reducer').default);
         return import('./BusDetail/container');
       },
       loading: () => <Spin />,
     });
     this.CarManage = lodable({
       loader: () => {
-        injectAsyncReducer(
-          context.store,
-          'buses',
-          require('./BusManagement/reducer').default,
-        );
+        injectAsyncReducer(context.store, 'buses', require('./BusManagement/reducer').default);
         return import('./BusManagement/container');
       },
       loading: () => <Spin />,
     });
     this.BusReport = lodable({
       loader: () => {
-        injectAsyncReducer(
-          context.store,
-          'busReport',
-          require('./BusReport/reducer'),
-        );
+        injectAsyncReducer(context.store, 'busReport', require('./BusReport/reducer'));
         return import('./BusReport/container');
       },
       loading: () => <Spin />,
@@ -80,6 +66,7 @@ class Router extends React.Component {
         <Layout>
           <Switch>
             <Route exact path="/" component={this.IndexPage} />
+            <Route exact path="/map" component={this.MapPage} />
             <Route exact path="/buses/monitor" component={this.BusMonitor} />
             <Route exact path="/buses/monitor/:vin" component={this.BusDetail} />
             <Route exact path="/buses" component={this.CarManage} />
