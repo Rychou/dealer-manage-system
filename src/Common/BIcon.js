06dff695966ba248a7debea8d24b7e07a83f9e1d
xxxsimons <974393900@qq.com>
2018-11-08 19:33:56 +0800

    [BIcon] 更新BIcon

--- src/Common/BIcon.js
+++ src/Common/BIcon.js
@@ -2,7 +2,7 @@ import { Icon } from 'antd';
 
 // 扩展Antd Icon，Battery-Icon缩写
 const BIcon = Icon.createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_881369_p7ak1sxfd9a.js', // 在 iconfont.cn 上生成
+  scriptUrl: '//at.alicdn.com/t/font_881369_vyit2n4dicb.js', // 在 iconfont.cn 上生成
 });
 
 export default BIcon;
