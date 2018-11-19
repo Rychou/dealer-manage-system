8d58b2e5d6cbcbce74c769006056d30fde1dbf2d
xxxsimons <974393900@qq.com>
2018-11-19 20:57:07 +0800

    [utils/config] 增加经销商和集团的menu配置和面包屑配置

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,67 +1,46 @@
 /*
  * 菜单配置参数
  */
-export const menus = [
+export const defaultMenus = [
   {
     isSub: false,
     name: '首页',
     icon: 'anticon-bulb',
     path: '/',
   },
+];
+
+// 经销商菜单
+export const dealerMenus = [
   {
     isSub: false,
-    name: '地图分布',
-    icon: 'anticon-location',
-    path: '/map',
+    name: '首页',
+    path: '/',
+    icon: 'home',
   },
   {
     isSub: false,
-    name: '车辆监控',
-    icon: 'anticon-alert',
-    path: '/buses/monitor',
-  },
-  {
-    isSub: false,
-    name: '车辆管理',
-    path: '/buses',
-    icon: 'anticon-deploymentunit',
-  },
-  {
-    isSub: false,
-    name: '统计分析',
-    path: '/buses/report',
-    icon: 'anticon-deploymentunit',
+    name: '商品列表',
+    path: '/products',
+    icon: 'shopping',
   },
 ];
 
-// 路由-面包屑映射列表
-export const breadcrumbNameMap = {
-  '/': { path: '/', exact: true, name: '首页' },
-  '/map': {
-    name: '地图分布',
-    path: '/map',
+// 集团菜单
+export const companyMenus = [];
+
+// 经销商 路由-面包屑映射列表
+export const dealerBreadcrumbNameMap = {
+  // '/': { path: '/', exact: true, name: '首页' },
+  '/': {
+    exact: true,
+    path: '/',
+    name: '首页',
   },
-  '/buses/monitor': {
-    name: '车辆监控',
-    path: '/buses/monitor',
-    component: '@/BusMonitor/container',
-    children: [
-      {
-        name: '车辆详情',
-        path: '/buses/monitor/:id',
-      },
-    ],
-  },
-  '/buses/monitor/:id': {
-    name: '车辆详情',
-    path: '/buses/monitor/:id',
-  },
-  '/buses': {
-    name: '车辆管理',
-    path: '/buses',
-  },
-  '/buses/report': {
-    name: '车辆报表',
-    path: '/buses/report',
+  '/products': {
+    path: '/products',
+    name: '商品列表',
   },
 };
+// 集团
+export const companyBreadcrumbNameMap = {};
